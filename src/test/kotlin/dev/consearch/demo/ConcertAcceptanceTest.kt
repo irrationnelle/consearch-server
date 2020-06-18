6dc4e6408e83662310a8cbb02d7d93a3756ef573
irrationnelle <drakkarverenis@gmail.com>
2020-06-18 23:20:28 +0900

    test: add retrieveConcerts test to fail

--- src/test/kotlin/dev/consearch/demo/ConcertAcceptanceTest.kt
+++ src/test/kotlin/dev/consearch/demo/ConcertAcceptanceTest.kt
@@ -2,6 +2,7 @@ package dev.consearch.demo
 
 import com.beust.klaxon.Klaxon
 import dev.consearch.demo.domain.Concert
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.web.reactive.server.EntityExchangeResult
 import org.springframework.test.web.reactive.server.WebTestClient
 import reactor.core.publisher.Mono
 
@@ -32,5 +34,46 @@ class ConcertAcceptanceTest {
             .exchange()
             .expectStatus()
             .isCreated()
+    }
+
+    @DisplayName("공연 목록 받아오기")
+    @Test
+    fun retrieveConcerts() {
+        // given
+        val firstConcert = Concert("Behemoth");
+        val firstInputJson = Klaxon().toJsonString(firstConcert)
+
+        webTestClient.post()
+            .uri("/concerts")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(Mono.just(firstInputJson), String::class.java)
+            .exchange()
+            .expectStatus()
+            .isCreated()
+
+        val secondConcert = Concert("Shining");
+        val secondInputJson = Klaxon().toJsonString(secondConcert)
+
+        webTestClient.post()
+            .uri("/concerts")
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(Mono.just(secondInputJson), String::class.java)
+            .exchange()
+            .expectStatus()
+            .isCreated()
+
+        // when
+        val response: EntityExchangeResult<List<Concert>> = webTestClient.get().uri("/concerts")
+            .exchange()
+            .expectStatus().isOk
+            .expectHeader().contentType(MediaType.APPLICATION_JSON)
+            .expectBodyList(Concert::class.java)
+            .returnResult()
+
+        // then
+
+        // then
+        assertThat(response.responseBody?.size).isEqualTo(2)
+
     }
 }
