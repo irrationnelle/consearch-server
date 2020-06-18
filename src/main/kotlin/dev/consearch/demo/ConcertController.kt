2bbe016ef27a4cdbdcc7143220d8f34939c030dd
irrationnelle <drakkarverenis@gmail.com>
2020-06-18 23:44:03 +0900

    feat: add retrieveConcerts function to pass test with static values

--- src/main/kotlin/dev/consearch/demo/ConcertController.kt
+++ src/main/kotlin/dev/consearch/demo/ConcertController.kt
@@ -2,6 +2,7 @@ package dev.consearch.demo
 
 import dev.consearch.demo.domain.Concert
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import java.net.URI
@@ -13,5 +14,14 @@ class ConcertController {
         return ResponseEntity
             .created(URI.create("/concerts/1"))
             .build()
+    }
+
+    @GetMapping("/concerts")
+    fun retrieveConcerts(): ResponseEntity<List<Concert>>? {
+        val firstConcert = Concert("Behemoth");
+        val secondConcert = Concert("Shining");
+        val persistStations: List<Concert> = listOf(firstConcert, secondConcert);
+
+        return ResponseEntity.ok().body(persistStations)
     }
 }
