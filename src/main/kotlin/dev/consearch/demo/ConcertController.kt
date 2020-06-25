69c82e57eb08c56a5fedb256abb7efc454f1d163
irrationnelle <drakkarverenis@gmail.com>
2020-06-25 23:42:55 +0900

    feat: implement retrieveConcert function for single detail get request

--- src/main/kotlin/dev/consearch/demo/ConcertController.kt
+++ src/main/kotlin/dev/consearch/demo/ConcertController.kt
@@ -3,10 +3,7 @@ package dev.consearch.demo
 import dev.consearch.demo.domain.Concert
 import dev.consearch.demo.repository.ConcertRepository
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.net.URI
 
 @RestController
@@ -14,13 +11,16 @@ class ConcertController(val concertRepository: ConcertRepository) {
 
     @PostMapping("/concerts")
     fun createStation(@RequestBody concert: Concert): ResponseEntity<Concert?> {
-        val persistConcert = concertRepository.save(concert);
+        val persistConcert = concertRepository.save(concert)
 
         return ResponseEntity
             .created(URI.create("/concerts/${persistConcert.id}"))
-            .body(concertRepository.save(concert));
+            .body(concertRepository.save(concert))
     }
 
     @GetMapping("/concerts")
-    fun retrieveConcerts(): Iterable<Concert?> = concertRepository.findAll();
+    fun retrieveConcerts(): Iterable<Concert?> = concertRepository.findAll()
+
+    @GetMapping("/concerts/{id}")
+    fun retrieveConcert(@PathVariable id: Long) = concertRepository.findById(id)
 }
