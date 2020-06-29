db86c7b9cfa7df33a3ccbf344d18771f354af40b
irrationnelle <drakkarverenis@gmail.com>
2020-06-29 23:52:24 +0900

    fix: set CascadeType.MERGE instead of CascadeType.ALL for many-to-many

--- src/main/kotlin/dev/consearch/demo/domain/Concert.kt
+++ src/main/kotlin/dev/consearch/demo/domain/Concert.kt
@@ -7,7 +7,7 @@ class Concert(val title: String,
               val address: String,
               val price: Int,
               val timetable: String,
-              @ManyToMany(fetch = FetchType.LAZY, cascade = [CascadeType.PERSIST, CascadeType.MERGE])
+              @ManyToMany(fetch = FetchType.LAZY, cascade = [CascadeType.MERGE])
               @JoinColumn(name = "artist_id")
               var artists: MutableList<Artist?>
 ) {
