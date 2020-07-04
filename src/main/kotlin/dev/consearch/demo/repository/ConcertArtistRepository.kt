cabfb40486cae53668c024730afcabfe3ffe812a
irrationnelle <drakkarverenis@gmail.com>
2020-07-04 18:15:49 +0900

    chore: delete ConcertArtist

--- src/main/kotlin/dev/consearch/demo/repository/ConcertArtistRepository.kt
+++
@@ -1,7 +0,0 @@
-package dev.consearch.demo.repository
-
-import dev.consearch.demo.domain.ConcertArtist
-import org.springframework.data.repository.CrudRepository
-
-interface ConcertArtistRepository : CrudRepository<ConcertArtist?, Long?> {
-}
