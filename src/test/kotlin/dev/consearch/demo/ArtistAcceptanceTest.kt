cb6e33d4861341b8ab1b3ace90b9b94e69ed047a
irrationnelle <drakkarverenis@gmail.com>
2020-07-04 19:58:28 +0900

    chore: delete SearchArtistRequestView

--- src/test/kotlin/dev/consearch/demo/ArtistAcceptanceTest.kt
+++ src/test/kotlin/dev/consearch/demo/ArtistAcceptanceTest.kt
@@ -1,7 +1,6 @@
 package dev.consearch.demo
 
 import dev.consearch.demo.application.dto.CreateArtistRequestView
-import dev.consearch.demo.application.dto.SearchArtistRequestView
 import dev.consearch.demo.domain.Artist
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
@@ -32,14 +31,11 @@ class ArtistAcceptanceTest {
         httpTest = HttpTest(webTestClient)
     }
 
-
     @DisplayName("아티스트 등록을 하지 않았는데 아티스트 이름으로 아티스트 정보 조회를 하면 404 statusCode 를 보여준다.")
     @Test
     fun retrieveArtistNotRegistered() {
-        val searchArtist = SearchArtistRequestView("Behemoth")
-
         val getWithQueryParameter = { uriBuilder: UriBuilder ->
-            uriBuilder.path("${domainUri}/").queryParam("name", searchArtist.name).build()
+            uriBuilder.path("${domainUri}/").queryParam("name", "Behemoth").build()
         }
 
         webTestClient.get().uri(getWithQueryParameter)
@@ -65,9 +61,8 @@ class ArtistAcceptanceTest {
         val createdArtist = httpTest.createRequest(artist, domainUri, Artist::class.java).responseBody
 
         // when
-        val searchArtist = SearchArtistRequestView(artist.name)
         val getWithQueryParameter = { uriBuilder: UriBuilder ->
-            uriBuilder.path("${domainUri}/").queryParam("name", searchArtist.name).build()
+            uriBuilder.path("${domainUri}/").queryParam("name", artist.name).build()
         }
         val responseArtist = httpTest.retrieveRequest(getWithQueryParameter, Artist::class.java).responseBody;
 
